9bf4c6b465de65b8c3b1d7fa7d00b732c1f4e0d6
Kristin Evans <kevans0625@gmail.com>
2020-03-09 13:45:45 -0500

    created server.js to run application

---
+++ server.js
@@ -0,0 +1,13 @@
+// Dependencies
+// =============================================================
+// Sets up the Express App
+// =============================================================
+// set up the express server 
+//parse the data 
+//create the html routes
+//send the user to the homepage
+//send the user to the notes page 
+//create a new note
+//store the data in db,json using fs 
+//create api routes 
+//server listener 
