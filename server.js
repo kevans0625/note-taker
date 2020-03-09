29bb658504e701c2e0a6417e05bd4b3dfd1ae358
Kristin Evans <kevans0625@gmail.com>
2020-03-09 13:47:43 -0500

    Set up server to listen on port 3000.

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 // Dependencies
 // =============================================================
+var express = require("express");
+
+
 // Sets up the Express App
 // =============================================================
+var app = express();
+var PORT = 3000;
 // set up the express server 
 //parse the data 
 //create the html routes
@@ -11,3 +16,7 @@
 //store the data in db,json using fs 
 //create api routes 
 //server listener 
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
