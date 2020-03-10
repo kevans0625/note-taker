d18c262da803082443a11513030797e6b6b3c6d6
Kristin Evans <kevans0625@gmail.com>
2020-03-09 21:17:19 -0500

    Added in ability to parse json data.

--- server.js
+++ server.js
@@ -4,30 +4,39 @@ var express = require("express");
 var path = require("path")
 
 
+
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = 8080;
 
 //parse the data 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //create the html routes
 // =============================================================
 
+//send the user to the homepage
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+//send the user to the notes page 
 app.get("/notes", function(req, res){
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
-//send the user to the homepage
-//send the user to the notes page 
+
+//review db.json
+
 //create a new note
+
 //store the data in db,json using fs 
+
 //create api routes 
+
 //server listener 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-  });
+ });
   
