0a09265e25b70ff4804735fbdf806fdacec3b1a0
Kristin Evans <kevans0625@gmail.com>
2020-03-14 21:36:53 -0500

    Updated the json file for get.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,49 @@
 var fs = require("fs");
-// var uuid = require("uuid");
 var notes =  require("../db/db.json")
+const { v4: uuidv4 } = require('uuid');
+
 //get db.json
 module.exports = function(app) {
-fs.readFile("./db/db.json", 'utf-8', (err, data) => {
-    if (err) throw err
+// fs.readFile("./db/db.json", 'utf-8', (err, data) => {
+//     if (err) throw err
   
-    notes = JSON.parse(data)
-  });
+//     notes = JSON.parse(data)
+//   });
 
 // * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 app.get("/api/notes", function(req, res) {
-    console.log("hello!");
+  console.log("hello!");
+  
+  fs.readFile("./db/db.json", 'utf-8', (err, data) => {
+    if (err) throw err
+  
+    notes = JSON.parse(data)
     return res.json(notes);
-    //How do you check to see if this is working? 
+  })
+  
   });
+
 
 //   //create a new note
   // * POST `/api/notes` - Should recieve a new note to save on the request body, add it to the
   //  `db.json` file, and then return the new note to the client.
+  // app.post("/api/notes", function(req, res) {
+  //   console.log("hey you!");
+  //   const newNote = req.body
+  //   newNote.id = uuidv4();
+  //   console.log(newNote);
+  //   notes.push(newNote);
+  //   fs
+
+  //   fs.writeFile("./db/db.json", notes, (err, data) => {
+  //     if (err) throw err
+    
+  //     notes = JSON.parse(data)
+  //   });
   
+  //   return res.json(notes);
+  // });
+
   // * DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete.
   //  This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete 
   //  a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property,
