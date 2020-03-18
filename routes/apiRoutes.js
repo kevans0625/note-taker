3ac21625bca075c4a759e35c696863b5363340a7
Kristin Evans <kevans0625@gmail.com>
2020-03-18 18:56:07 -0500

    created the app/post method.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,73 +1,71 @@
 var fs = require("fs");
-var notes =  require("../db/db.json")
+var notes = require("../db/db.json")
 const { v4: uuidv4 } = require('uuid');
 
 //get db.json
-module.exports = function(app) {
-// fs.readFile("./db/db.json", 'utf-8', (err, data) => {
-//     if (err) throw err
-  
-//     notes = JSON.parse(data)
-//   });
+module.exports = function (app) {
+  // fs.readFile("./db/db.json", 'utf-8', (err, data) => {
+  //     if (err) throw err
 
-// * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
-app.get("/api/notes", function(req, res) {
-  console.log("hello!");
-  
-  fs.readFile("./db/db.json", 'utf-8', (err, data) => {
-    if (err) throw err
-  
-    notes = JSON.parse(data)
-    return res.json(notes);
-  })
-  
+  //     notes = JSON.parse(data)
+  //   });
+
+  // * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+  app.get("/api/notes", function (req, res) {
+    console.log("hello!");
+
+    fs.readFile("./db/db.json", 'utf-8', (err, data) => {
+      if (err) throw err
+      //add an id 
+      notes = JSON.parse(data)
+      return res.json(notes);
+    })
+
   });
 
 
-//   //create a new note
+  //   //create a new note
   // * POST `/api/notes` - Should recieve a new note to save on the request body, add it to the
   //  `db.json` file, and then return the new note to the client.
-  // app.post("/api/notes", function(req, res) {
-  //   console.log("hey you!");
-  //   const newNote = req.body
-  //   newNote.id = uuidv4();
-  //   console.log(newNote);
-  //   notes.push(newNote);
-  //   fs
+  app.post("/api/notes", function (req, res) {
+    //request holds data from the client 
+    console.log(req);
+    var note_title = req.body.title;
+    var note_content = req.body.text;
+    //add id as a property of note everytime a note is added 
 
-  //   fs.writeFile("./db/db.json", notes, (err, data) => {
-  //     if (err) throw err
+    fs.readFile('./db/db.json', 'utf-8', function (err, data) {
+      if (err) throw err
+      let max_number = 0;
+      var arrayOfObjects = JSON.parse(data)
+      for (let i =0; i < arrayOfObjects.length; i++){
+        if (arrayOfObjects.length == 1 ){
+          max_number = arrayOfObjects[i].id;
+        } else if (arrayOfObjects[i].id > arrayOfObjects[i+1].id) {
+          max_number = arrayOfObjects[i].id;
+        } else if (arrayOfObjects[i].id < arrayOfObjects[i+1].id){
+          max_number = arrayOfObjects[i+1].id;
+        }
+      }
+      console.log(arrayOfObjects)
+      arrayOfObjects.push({
+        id: max_number+1,
+        title: note_title,
+        text: note_content
+      })
+
+
+
+      fs.writeFile('./db/db.json', JSON.stringify(arrayOfObjects), 'utf-8', function (err) {
+        if (err) throw err
+        console.log('Done!')
+      })
+    })
+  });
+
+};
+
+
     
-  //     notes = JSON.parse(data)
-  //   });
+    
   
-  //   return res.json(notes);
-  // });
-
-  // * DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete.
-  //  This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete 
-  //  a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property,
-  //   and then rewrite the notes to the `db.json` file.
-  
-  // app.delete("/api/notes/:id", function(req, res) {
-  //     // req.body hosts is equal to the JSON post sent from the user
-  //     // This works because of our body parsing middleware 
-  //create a loop 
-  //     notes.splice(oldNote);
-
-    // if (activeNote.id === note.id) {
-    //   activeNote = {};
-    // }
-  
-    // deleteNote(note.id).then(function() {
-    //   getAndRenderNotes();
-    //   renderActiveNote();
-    // });
-  //   });
-
-//store the data in db,json using fs 
-
-//delete the data in db,json using fs 
-
-//create api routes 
-}
