a7e224b1426ebacbf31f87ec737883133e6693e4
Kristin Evans <kevans0625@gmail.com>
2020-03-18 19:16:29 -0500

    updated port settings

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 8080;
+
+var PORT = process.env.PORT || 3000;
 
 //parse the data 
 app.use(express.urlencoded({ extended: true }));
